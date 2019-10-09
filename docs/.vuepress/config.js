70988923e4ad95c8966fc29ab507efe61980343c
Astrian Zheng <mingriqd@gmail.com>
2019-10-09 21:23:38 +0800

    增加一些 PAFF 发布的 posts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,14 @@ module.exports = {
   description: '以时间线及人物为线索，整理 Cytus II 相关剧情及设定。',
   lastUpdated: '本文更新于',
   themeConfig: {
+    nav: [
+      { text: 'iM', link: '/iM/' },
+    ],
     sidebar: {
+      '/iM/': [
+        ['', '公共时间轴'],
+        ['PAFF-Official/', 'PAFF-Official']
+      ],
       '/': []
     }
   }
