e12b1a2511731c4234e1c5b59397786c30d10220
Astrian Zheng <mingriqd@gmail.com>
2019-10-09 21:29:35 +0800

    更新时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'Cytus II 剧情线情报站',
   description: '以时间线及人物为线索，整理 Cytus II 相关剧情及设定。',
-  lastUpdated: '本文更新于',
   themeConfig: {
+    lastUpdated: '本文更新于',
     nav: [
       { text: 'iM', link: '/iM/' },
       { text: '角色文档', link: '/databases/' },
